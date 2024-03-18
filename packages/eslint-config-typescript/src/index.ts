c61c5ab4a3e6521ef540e1cb5181f7281df084e5
Luna Simons <luna@bddvlpr.com>
2024-03-18 13:46:37 +0100

    feat: added import linting for typescript

--- packages/eslint-config-typescript/src/index.ts
+++ packages/eslint-config-typescript/src/index.ts
@@ -1,11 +1,37 @@
 import type { Linter } from "eslint";
 
 export = {
-  extends: ["plugin:@typescript-eslint/recommended"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+  ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "import"],
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020,
+  },
+  rules: {
+    "import/no-unresolved": "off",
+    "import/no-self-import": "error",
+    "import/order": [
+      "warn",
+      {
+        "newlines-between": "always",
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
   },
 } as Linter.Config;
